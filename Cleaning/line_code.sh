3ab26001a3eb7b4106ccf6f19e49f5966967149c
Karina Vazquez <karina_alejandra989@hotmail.com>
2016-11-16 16:51:20 -0700

    Create line_code.sh

---
+++ Cleaning/line_code.sh
@@ -0,0 +1,10 @@
+#This is a test for the command line format
+
+pwd #to see the path from home to the directory where you are
+
+mkdir ./home/RADseq/data
+mkdir ./home/RADseq/data/working
+mkdir ./home/RADseq/data/working/clean
+
+tar -xvf /path/to/file/lane1.tar -C /path/to/directory/
+ls -l 
